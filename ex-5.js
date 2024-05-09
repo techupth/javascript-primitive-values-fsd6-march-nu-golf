59cb6bf5e9e980f73913e6e7a3d88d4f9e3f21e4
Athikhom_ra <athikhom_ra@outlook.com>
2024-05-09 15:07:18 +0700

    Assignment#2 NARu

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrices = 27000;
+let promotionDiscount = `${totalProductPrices * (25 / 100)}`;
+let priceNet = `${totalProductPrices - promotionDiscount}`;
